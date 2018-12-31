6d9f2f56b4f6265f1356c4f7247be24b6b7673f9
Patrick <patrick.kurzeja@gmail.com>
2018-12-31 21:58:14 +0100

    Create basic Vue view

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About</h1>
   </div>
 </template>
+
+<script>
+export default {
+  name: "About"
+};
+</script>
+<style lang="scss" scoped>
+
+</style>
